6ffcd865d48b8fbe8ca3ed17ae8ffd7d6da69a03
Copons <Copons@users.noreply.github.com>
2021-03-19 10:23:28 +0000

    Fix typo

--- packages/block-editor/src/components/block-navigation/index.js
+++ packages/block-editor/src/components/block-navigation/index.js
@@ -30,7 +30,7 @@ function BlockNavigation( {
 
 	const hasHierarchy =
 		rootBlock &&
-		( isClientIdSelected( rootBlock.clientId, selectedBlockClientId ) ||
+		( ! isClientIdSelected( rootBlock.clientId, selectedBlockClientId ) ||
 			( rootBlock.innerBlocks && rootBlock.innerBlocks.length !== 0 ) );
 
 	return (
